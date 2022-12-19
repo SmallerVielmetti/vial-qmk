0404bc4806dd6db221b10b81ebfff2457ab07283
Jonathan Vielmetti <25182969+SmallerVielmetti@users.noreply.github.com>
2022-12-19 02:46:52 -0500

    adding uno vial file

---
+++ keyboards/keyhive/uno/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xCE, 0x32, 0x61, 0x30, 0x72, 0x0A, 0xE0, 0xF8}
+#define ENCODERS_PAD_A { B2 }
+#define ENCODERS_PAD_B { B3 }
+#define ENCODER_DIRECTION_FLIP
---
+++ keyboards/keyhive/uno/keymaps/vial/readme.md
@@ -0,0 +1,3 @@
+I am struggling
+like damn I'm not good at this
+if this is changed or you're reading this later, whoops
