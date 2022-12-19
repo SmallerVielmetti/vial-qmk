27e50328640de37f5c21b10a9e0e88b2b55dc2bb
Jonathan Vielmetti <25182969+SmallerVielmetti@users.noreply.github.com>
2022-12-19 02:44:16 -0500

    adding vial keymap

---
+++ keyboards/1upkeyboards/1up60rgb/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x28, 0x44, 0x9F, 0x2B, 0x06, 0x8D, 0x2B, 0x74}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
